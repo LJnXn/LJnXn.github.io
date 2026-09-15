9df4d6efdb44d6f46c33493d4cf8bb967f9f8fc6
DZTsun <64788290+LJnXn@users.noreply.github.com>
2026-09-15 16:10:10 +0800

    Render LaTeX formulas with KaTeX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import { unified } from "@astrojs/markdown-remark";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 export default defineConfig({
   site: "https://ljnxn.github.io",
   output: "static",
   integrations: [sitemap()],
+  markdown: {
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [[rehypeKatex, { output: "htmlAndMathml", strict: "warn" }]],
+    }),
+  },
 });
